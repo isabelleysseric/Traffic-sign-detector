5a93d3dbb87b021209076df575bce2a99dee8518
Benoit Verret <benoit.verret.1988@gmail.com>
2020-12-14 20:34:52 -0500

    Training images reader snippet

---
+++ readTrafficSigns.py
@@ -0,0 +1,44 @@
+# The German Traffic Sign Recognition Benchmark
+#
+# sample code for reading the traffic sign images and the
+# corresponding labels
+#
+# example:
+#            
+# trainImages, trainLabels = readTrafficSigns('GTSRB/Training')
+# print len(trainLabels), len(trainImages)
+# plt.imshow(trainImages[42])
+# plt.show()
+#
+# have fun, Christian
+
+import matplotlib.pyplot as plt
+import csv
+from pathlib import PurePath
+
+ROOTPATH_TESTING = PurePath("GTSRB", "testing", "images")
+ROOTPATH_TRAINING = PurePath("GTSRB", "training", "images")
+# function for reading the images
+# arguments: path to the traffic sign data, for example './GTSRB/Training'
+# returns: list of images, list of corresponding labels 
+def readTrainingTrafficSigns(rootpath):
+    '''Reads traffic sign data for German Traffic Sign Recognition Benchmark.
+
+    Arguments: path to the traffic sign data, for example './GTSRB/Training'
+    Returns:   list of images, list of corresponding labels'''
+    images = [] # images
+    labels = [] # corresponding labels
+    # loop over all 42 classes
+    for c in range(0,43):
+        prefix = PurePath(rootpath, f"{c:05d}") # subdirectory for class
+        gtFile = open(PurePath(prefix, f"GT-{c:05d}.csv")) # annotations file
+        gtReader = csv.reader(gtFile, delimiter=';') # csv parser for annotations file
+        next(gtReader) # skip header
+        # loop over all images in current annotations file
+        for row in gtReader:
+            images.append(plt.imread(str(PurePath(prefix, row[0])))) # the 1th column is the filename
+            labels.append(row[7]) # the 8th column is the label
+        gtFile.close()
+    return images, labels
+
+readTrainingTrafficSigns(ROOTPATH_TRAINING)
